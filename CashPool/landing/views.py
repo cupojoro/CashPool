23b494b20d2abc557a3617416dfde1df8cfd44f5
hendrixjonathan1 <95508804+hendrixjonathan1@users.noreply.github.com>
2022-04-25 14:03:41 -0700

    Wrote a path to the main page.

--- CashPool/landing/views.py
+++ CashPool/landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("This is set ut to make sure that you are at the landing page.")
